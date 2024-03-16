226ffc1292e87f0bfeb2e2424a5883b26553b249
sixpetallilac <treecheese@163.com>
2024-03-16 16:23:22 +0800

    final ver

--- springcache-demo/src/main/java/com/itheima/controller/UserController.java
+++ springcache-demo/src/main/java/com/itheima/controller/UserController.java
@@ -4,6 +4,9 @@ import com.itheima.entity.User;
 import com.itheima.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,22 +18,30 @@ public class UserController {
     private UserMapper userMapper;
 
     @PostMapping
+    @CachePut(cacheNames = "userCache", key = "#user.id")
+//    @CachePut(cacheNames = "userCache", key = "#result.id")
+//    @CachePut(cacheNames = "userCache", key = "#a0.id")
+//    @CachePut(cacheNames = "userCache", key = "#p0.id")
+//    @CachePut(cacheNames = "userCache", key = "#root.args[0].id")
     public User save(@RequestBody User user){
         userMapper.insert(user);
         return user;
     }
 
     @DeleteMapping
+    @CacheEvict(cacheNames = "userCache", key = "#id")
     public void deleteById(Long id){
         userMapper.deleteById(id);
     }
 
 	@DeleteMapping("/delAll")
+    @CacheEvict(cacheNames = "userCache",allEntries = true)
     public void deleteAll(){
         userMapper.deleteAll();
     }
 
     @GetMapping
+    @Cacheable(cacheNames = "userCache",key = "#id")
     public User getById(Long id){
         User user = userMapper.getById(id);
         return user;
